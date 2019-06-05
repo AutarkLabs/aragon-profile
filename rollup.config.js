780f2823279a18b501510bd3d062e54a88b278e2
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2019-06-05 08:13:52 -0400

    more bundling changes, still not working

--- rollup.config.js
+++ rollup.config.js
@@ -5,28 +5,25 @@ import url from 'rollup-plugin-url'
 import filesize from 'rollup-plugin-filesize'
 import json from 'rollup-plugin-json'
 import progress from 'rollup-plugin-progress'
+import builtins from 'rollup-plugin-node-builtins';
 import pkg from './package.json'
 
 export default {
   input: 'src/index.js',
-  output: [
-    { file: pkg.main, format: 'cjs' },
-    { file: pkg.module, format: 'es' },
-  ],
+  output: [{ file: pkg.main, name: 'index.js', format: 'iife' }],
   external: ['react', 'react-dom', 'styled-components'],
+  globals: {
+    react: 'React',
+    'styled-component': 'styled',
+  },
   plugins: [
     progress(),
+    json(),
+    builtins(),
     url({
       limit: 5 * 1024, // inline files smaller than 5k
       publicPath: '',
-      include: [
-        '**/*.svg',
-        '**/*.png',
-        '**/*.jpg',
-        '**/*.gif',
-        '**/*.woff',
-        '**/*.woff2',
-      ],
+      include: ['**/*.svg', '**/*.png', '**/*.jpg'],
       emitFiles: true,
     }),
     babel({

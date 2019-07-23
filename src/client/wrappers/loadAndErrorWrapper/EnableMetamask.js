2e1c35ae2ab705bff64114882a9e7f704195ddf0
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2019-07-23 18:11:49 -0400

    add enable metamask screen

---
+++ src/client/wrappers/loadAndErrorWrapper/EnableMetamask.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { EmptyStateCard, unselectable, IconIdentity } from '@aragon/ui'
+
+const EnableMetamask = ({ enableWallet }) => (
+  <EmptyWrapper>
+    <EmptyStateCard
+      title="You haven't enabled your account"
+      text="Get started now by creating a new account."
+      icon={<IconIdentity />}
+      actionText="Enable account"
+      onActivate={enableWallet}
+    />
+  </EmptyWrapper>
+)
+
+EnableMetamask.propTypes = {
+  enableWallet: PropTypes.func.isRequired,
+}
+
+const EmptyWrapper = styled.div`
+  ${unselectable};
+  margin-top: 50px;
+  flex-grow: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+export default EnableMetamask
